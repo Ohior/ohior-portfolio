02ff3b18d128b8b2889cfeed6faef089fae22a7f
Ohiorenua <50307290+Ohior@users.noreply.github.com>
2025-06-24 07:20:39 +0100

    added trans tiggle

---
+++ scripts/tictactoe.js
@@ -0,0 +1,13 @@
+const tictactoe_div = document.getElementById("tictactoe-game");
+const table = document.createElement("table");
+
+tictactoe_div.append(table);
+
+
+// draw table
+function drawTableBox(table_nums) {
+    for (let index = 0; index < table_nums.length; index++) {
+        const element = table_nums[index];
+        
+    }
+}
